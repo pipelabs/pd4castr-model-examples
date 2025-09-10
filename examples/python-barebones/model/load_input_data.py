8748f14b5d2c3e224eebd96e31f0732aaad0b52c
Geoff Whatley <me@geoffwhatley.com>
2025-09-10 12:06:11 +1000

    feat: add minimum viable python barebones project

---
+++ examples/python-barebones/model/load_input_data.py
@@ -0,0 +1,12 @@
+def load_input_data():
+    """
+    Load input data from environment variables and download from URLs.
+
+    Returns:
+        tuple: (input_data) - the input data from each input source
+    """
+
+    # TODO: Load your input data from environment variables
+    input_data = {}
+
+    return input_data
---
+++ examples/python-barebones/model/run_model.py
@@ -0,0 +1,16 @@
+def run_model(input_data):
+    """
+    Run the model with your input data.
+
+    Args:
+        input_data: The input data for the model
+
+    Returns:
+        list: List of dictionaries containing the model output
+    """
+
+    # TODO: Run the model with your input data and return the results
+    results = []
+
+    return results
+
